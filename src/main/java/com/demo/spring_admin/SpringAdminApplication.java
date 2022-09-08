02be6cea5b2d9e7ef21efacf75e29c9a338a6e24
yusanghuck <g01099387636@gmail.com>
2022-09-08 15:22:11 +0900

    add spring boot admin

--- src/main/java/com/demo/spring_admin/SpringAdminApplication.java
+++ src/main/java/com/demo/spring_admin/SpringAdminApplication.java
@@ -1,9 +1,11 @@
 package com.demo.spring_admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringAdminApplication {
 
     public static void main(String[] args) {
